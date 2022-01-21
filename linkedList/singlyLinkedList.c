86ccb87542c24d03f1c37c99057b2b59f1b42415
PiyushDuggal-source <duggalpiyush0@gmail.com>
2022-01-22 00:58:55 +0530

    22 jan 2022

--- linkedList/singlyLinkedList.c
+++ linkedList/singlyLinkedList.c
@@ -1,44 +1,82 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct node {
-int data;
-struct node *next;
+struct node
+{
+    int data;
+    struct node *next;
 };
 struct node *head, *temp, *newNode;
-int choice, length;
-void create(){
-    printf("Enter 0 to add or any number to create: ");
-    scanf("%d", choice);
-    while(choice != 0){
+
+int create()
+{
+    int choice, length = 0;
+
+    printf("Enter 0 to stop or any number to create: ");
+    scanf("%d", &choice);
+    while (choice != 0)
+    {
         length++;
         newNode = (struct node *)malloc(sizeof(struct node));
         printf("Enter the element: ");
-        scanf("%d" , &newNode->data);
+        scanf("%d", &newNode->data);
         newNode->next = 0;
-        if(head == NULL){
+        if (head == 0)
+        {
             head = newNode;
             temp = head;
-
         }
-        temp->next = newNode;
-        temp= newNode;
-        printf("Enter 0 to add or any number to create: ");
-    scanf("%d", choice);
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
 
+        printf("Enter 0 to stop or any number to create: ");
+        scanf("%d", &choice);
     }
 
-    temp = head;
-    for(int i = 0; i<length; i++){
-        printf("%d", temp->data);
-        temp->next;
-    }
-
-
-    
+    return length;
 }
 
+int addAtTheEnd()
+{
+    newNode = (struct node *)malloc(sizeof(struct node));
+    printf("Enter the element: ");
+    scanf("%d", &newNode->data);
+    int len = 0;
+    printf("%d", temp);
+    temp = head;
+    printf("%d", head);
 
-int main(){
+    while (temp != 0)
+    {
+        len++;
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    printf("\n%d", len);
+
+    return len;
+}
+
+void display(int length)
+{
+    temp = head;
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    int length = create();
     create();
+    display(length);
+    addAtTheEnd();
+    display(length + 1);
+
+    return 0;
 }
